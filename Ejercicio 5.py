79928b6e351493a3388e5529ae314f87be9a119a
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-13 19:56:31 +0100

    Ej 5: Crear la lista de tareas

---
+++ Ejercicio 5.py
@@ -0,0 +1,8 @@
+#Ejercicio 5: Ordenación topológica de tareas
+import random
+
+class Tareas():
+        self.t.append("Ponerse los calcentines")
+        self.t.append("Ponerse las zapatillas")
+        self.t.append("Atarse los cordones")
+        random.shuffle(self.t)
---
+++ Ejercicio 6.py
@@ -0,0 +1,2 @@
+#Ejercicio 6: Especificación de está_explorado
+
