705b5365d9d904bb89aec59bfac4dd3ea0555d22
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-13 20:35:33 +0100

    Ej 5: Crear las tareas

---
+++ Ejercicio 5 de otra forma.py
@@ -0,0 +1,10 @@
+def ordenarTareas():
+
+
+    tareas = [1,2,3,4,5]
+    tareas=["ponerse los calcetines", "ponerse las zapatillas", "atarse los cordones", "coger las llaves", "salir de casa"]
+    lista = [(1,3), (3,5)]
+    
+
+
+
