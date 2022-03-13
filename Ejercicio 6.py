213f2c456c81cc2db0d52af46ae0994a57a2430d
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-13 20:56:18 +0100

    Ej 6: Creamos una lista aleatoria

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,2 +1,10 @@
 #Ejercicio 6: Especificación de está_explorado
+import random
+lista = []
+def creacion_lista():
+    repe = random.randint(5, 15)
+    for i in range(repe):
+        num = random.randint(0, 10)
+        lista.append(num)
+    return lista
 
