63b8144bd86fb73b108a9d48cd886876550f10f7
crltsnch <91721777+crltsnch@users.noreply.github.com>
2022-03-13 20:37:00 +0100

    Ej 5 acabado: no sé si está bien

--- Ejercicio 5 de otra forma.py
+++ Ejercicio 5 de otra forma.py
@@ -4,7 +4,10 @@ def ordenarTareas():
     tareas = [1,2,3,4,5]
     tareas=["ponerse los calcetines", "ponerse las zapatillas", "atarse los cordones", "coger las llaves", "salir de casa"]
     lista = [(1,3), (3,5)]
-    
 
+    ordenada[]
+    from Clases import ord
+    ordenada = list(dict.fromkeys(ordenada))
+    print(ordenada)
 
-
+ordenarTareas()
